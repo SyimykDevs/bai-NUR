0fd1a024dbd8d21bc5470ee0adc5611cc6ad0877
sYimyk3 <syim7k.dev@gmail.com>
2023-09-12 17:28:21 +0600

    basket

--- src/Pages/Basket/BasketPage.jsx
+++ src/Pages/Basket/BasketPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+
 import './BasketPage.css'
+import Restangle from './image/Rectangle 9.png'
 
 
 const BasketPage = () => {
+    const [counter, setCounter] = useState(0);
   return (
+
+    <>
+     
     <div className='section'>
         <div className="container">
             <div className="basket-content">
@@ -13,6 +20,50 @@ const BasketPage = () => {
             </div>
         </div>
     </div>
+
+
+    
+    <div className="basketContentAll">
+        <div className="container">
+            <div className="basket-section">
+                <div className="boxes">
+                    <div className="box-text">
+                  <div className="box1">
+                        <img src={Restangle} alt="" />
+                        <div className="box-content">
+                       <h4>Детская Футболка</h4>
+                        <p>Nike Sportswear Club+ Button-Down <br />
+                            Short-Sleeve Gömlek</p>
+                            <h6>Цена: 71$</h6>
+                            <div className='counter-container'>
+         <button onClick={() => setCounter(counter + 1)}>+</button>
+         <button onClick={() => setCounter(counter - 1)}>-</button>
+         <p>Count: {counter}</p>
+      </div>
+                            </div>
+                      </div>
+                    <h3>Всего: 142$</h3>
+                  </div>
+                   <div className="box2">
+                    <img src={Restangle} alt="" />
+                    <div className="box-content">
+                       <h4>Детская Футболка</h4>
+                        <p>Nike Sportswear Club+ Button-Down <br />
+                            Short-Sleeve Gömlek</p>
+                            <h6>Цена: 71$</h6>
+                            <div className='counter-container'>
+         <button onClick={() => setCounter(counter + 1)}>+</button>
+         <button onClick={() => setCounter(counter - 1)}>-</button>
+         <p>Count: {counter}</p>
+      </div>
+                            
+                     </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
