1745e392792b32c5911eff0d768b8daa85b68ede
sYimyk3 <syim7k.dev@gmail.com>
2023-09-12 14:48:01 +0600

    about

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -3,11 +3,33 @@ import './AboutPage.css'
 
 const About = () => {
   return (
-    <div>
-      <section className='section'>
-              
-      </section>
-    </div>
+   <div>
+    <section className='section'>
+      <div className="container">
+        <div className="contentAll">
+          <div className="about-content">
+            <h2>О нас </h2>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section className="section2">
+      <div className="container">
+        <div className="aboutAll">
+          <div className="content">
+          <h2 className='content-h2'>BAINUR</h2>
+          <p>
+            <a href="">Bai-Nur</a> - это интернет-магазин спортивной одежды и обуви. Мы продаем только оригинальные вещи, обувь и<br />   аксессуары от     ведущих спортивных брендов - Nike, Adidas, Puma, Reebok, VANS и др. На данный момент мы возим <br/> товары только на заказ. Мы доставляем     вещи из Турции, США, России. <br />
+    Мы выкупаем товары с официальных сайтов брендов и их партнеров. Каждого поставщика мы тщательно проверяем. <br />
+     На данном сайте товары     собираются с онлайн магазинов в автоматизированном режиме. <br />
+    Также мы выкупаем с локальных аутлетов и выставляем их на нашей страничке в <a href="">Instagram</a> 
+    <br /> 
+          </p>
+          </div>
+        </div>    
+      </div>
+    </section>
+  </div>
   )
 }
 
--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -8,9 +8,6 @@ const Home = () => {
                 <div className="text-background">
                     <h2>Главная </h2>
                <h3><span className='span-blue'>Главная </span> // <span>  Все Товары</span> </h3>
-               <p>Helllo</p>
-               <p>Helllo</p>
-               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non atque explicabo nesciunt soluta cum recusandae ipsa. Mollitia, aliquid fugit quos temporibus hic, sequi eos amet esse ratione consequuntur in laudantium? Quisquam dolorum eos illum eum impedit, debitis expedita amet eius vitae animi repellat qui molestiae odit quibusdam, fuga optio aut dignissimos blanditiis sequi ea odio laborum? Sint, a? Maxime tempora ipsam eligendi consectetur maiores atque vel rem voluptate impedit repudiandae natus voluptatum ducimus incidunt consequatur, quod quis cumque error pariatur eos nostrum dolorum eaque! Perferendis repellat, consectetur voluptatum saepe deleniti labore sint sapiente hic velit fugiat nam repellendus necessitatibus neque!</p>
                 </div>
             </div>
         </div>
