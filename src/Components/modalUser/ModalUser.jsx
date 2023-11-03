25833b69bd9036fe741d477b62b469d43a74b464
sancho002 <sanjarkairatkeldiev@gmail.com>
2023-11-03 11:41:08 +0300

    eyes

--- src/Components/modalUser/ModalUser.jsx
+++ src/Components/modalUser/ModalUser.jsx
@@ -1,12 +1,15 @@
-// import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineClose } from "react-icons/ai";
 import Facebook from '../images/Vector.png';
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import Google from '../images/logos_google-icon.png';
 
 
 import "./ModalUser.css";
 
 const ModalUser = ({ setModal }) => {
+
+ const [eye, setEye] = useState('')
   return (
     <div>
       <div className="modal__wrapper">
@@ -29,9 +32,15 @@ const ModalUser = ({ setModal }) => {
               <p>
                 <input
                   className="input-modal"
-                  type="password"
+                  type={eye ? 'text' : 'password'}
                   placeholder="password"
                 />
+                <span className="form__eye" onClick={()=> setEye(prev => !prev)}>
+                  {
+                    eye ?   <AiFillEyeInvisible /> : <AiFillEye />
+                  }
+                  
+                </span>
               </p>
             </div>
               <div className="liner-box">
